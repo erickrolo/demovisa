3eecb6e885b8f374c5f4e2d7d07a63e77820805d
Erick Rojas Lopez <2691615@tcs.com>
2026-03-30 14:21:09 -0600

    Initial commit

---
+++ src/main/java/com/example/demovisa/DemovisaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demovisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemovisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemovisaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demovisa/DemovisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demovisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemovisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
